3f9e3298e63573b282fcdb1979fad09e9eb7f6c3
Patryk Pastuszka <nov11157@gmail.com>
2025-09-02 22:18:58 +0200

    OperationResult improvements + check methods.

--- Operations/EconomyOperations.cs
+++ Operations/EconomyOperations.cs
@@ -4,12 +4,20 @@ namespace Systems.SimpleEconomy.Operations
 {
     public static class EconomyOperations
     {
-        public const int NOT_ENOUGH_CURRENCY = 1;
-        
-        public static OperationResult NotEnoughCurrency() => new(NOT_ENOUGH_CURRENCY);
-        public static OperationResult CurrencyAdded() => OperationResult.GenericSuccess;
-        public static OperationResult CurrencyTaken() => OperationResult.GenericSuccess;
-        
-        public static OperationResult Permitted() => OperationResult.GenericSuccess;
+        public const ushort SYSTEM_ECONOMY = 0x0001;
+
+
+        public const ushort ERROR_NOT_ENOUGH_CURRENCY = 1;
+
+        public const ushort SUCCESS_CURRENCY_ADDED = 1;
+        public const ushort SUCCESS_CURRENCY_TAKEN = 2;
+    
+        public static OperationResult NotEnoughCurrency()
+            => OperationResult.Error(SYSTEM_ECONOMY, ERROR_NOT_ENOUGH_CURRENCY);
+
+        public static OperationResult CurrencyAdded() => OperationResult.Success(SYSTEM_ECONOMY, SUCCESS_CURRENCY_ADDED);
+        public static OperationResult CurrencyTaken() => OperationResult.Success(SYSTEM_ECONOMY, SUCCESS_CURRENCY_TAKEN);
+
+        public static OperationResult Permitted() => OperationResult.Success(SYSTEM_ECONOMY, OperationResult.SUCCESS_PERMITTED);
     }
 }
